0f52e5c5f8f0e531ea74a435057ae7c08d5390a5
Sam Abrahms <abrahms.sam@gmail.com>
2015-07-21 17:44:38 -0500

    Completed animate with events assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,22 +3,25 @@ var pointsArray = document.getElementsByClassName('point');
 var animatePoints = function (points) {
                 
                 
-    var revealPoint = function (i) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function (point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
     }                
     
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    forEach(points, revealPoint);
+    
 };
+
+//forEach([1,2,3], function (element) {
+//    console.log(element);
+//});
 
 window.onload = function() {
     
     if (window.innerHeight > 950) {
-        animatePoints(animateArray);
+        animatePoints(animatePoints);
     }
     
     window.addEventListener('scroll', function(event) {
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+var forEach = function (theArray, callback) {
+    for (var i=0; i < theArray.length; i++) {
+        callback(theArray[i]);
+    }
+};
