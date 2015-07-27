d51d953526f42e1ff901981a4438d6fa53721a15
Sam Abrahms <abrahms.sam@gmail.com>
2015-07-27 08:57:06 -0500

    Updated album view with DOM scripting checkpoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,16 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function (point) {
+var animatePoints = function (points) {
                 
                 
     var revealPoint = function (i) {
-        point[i].style.opacity = 1;
-        point[i].style.transform = "scaleX(1) translateY(0)";
-        point[i].style.msTransform = "scaleX(1) translateY(0)";
-        point[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     }                
     
-    for (var i = 0; i < point.length; i++) {
+    for (var i = 0; i < points.length; i++) {
         revealPoint(i);
     }
 };
